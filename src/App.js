dd882eefbca4483a5b5d3f7197fa0c60f3b0e639
MJ Cariso <mjcariso@gmail.com>
2022-09-11 17:22:58 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+import './App.css';
+import './TitleData';
+import { useState } from 'react';
+import TitleData from './TitleData';
+
+function App() {
+  
+  const [Title, setTitle ]= useState(TitleData[0])
+  
+  const getTitle=()=>{
+    const Randomnumber=Math.floor(Math.random()*TitleData.length);
+    setTitle(TitleData[Randomnumber])
+  }
+  return (
+   
+   <>
+  <div className='container'>
+   <div className='pagetitle'>
+      <h1>Generate your thesis title</h1>
+      <div className='under'></div>
+    </div>
+   
+   <div className='card'>
+
+    <div className='title'>
+      <p>{Title.thesistitle}</p>
+     
+    
+    </div>
+    </div>
+    <div className='button'>
+        <button onClick={getTitle}>Generate</button>
+      </div>
+      <footer>
+      <p className='footer'>Made by <a href="https://www.facebook.com/mjcariso03"> MJ Cariso</a></p>
+      </footer>
+       
+     
+    </div>
+   
+   </>
+  );
+}
+
+export default App;
---
+++ src/TitleData.js
@@ -0,0 +1,154 @@
+
+//*Data*//
+const TitleData=[
+{
+    thesistitle:"Personal Cross-Media Information Management"
+},
+{
+    thesistitle:"Traffic Parking Management and Parking Lot System"
+},
+{
+    thesistitle:"Improving Nursing Education With a Healthcare System"
+},
+{
+    thesistitle:"Alarm SMS Notification Systems"
+},
+{
+    thesistitle:"Latest Ecommerce Technologies"
+},
+{
+    thesistitle:"Smart Technology in the Hotel Industry"
+},
+{
+    thesistitle:"3D Rendering Tools"
+},
+{
+    thesistitle:"Apps for Child Monitoring"
+},
+{
+    thesistitle:"Hospital System"
+},
+{
+    thesistitle:"An Interactive History and Geography of Mexico Using Map Objects for Java"
+},
+{
+    thesistitle:" Android Mobile Quiz Game"
+},
+{
+    thesistitle:"Application of Random Forest Algorithm in Biomarker Discovery for Cancer Detection"
+},
+{
+    thesistitle:"Electronic Police Clearance System"
+},
+{
+    thesistitle:"Dictionary Program PHP/MySQL Android"
+},
+{
+    thesistitle:"Medical records system"
+},
+{
+    thesistitle:"Information flow analysis for mobile applications"
+},
+{
+    thesistitle:" Hotel reservation system"
+},
+{
+    thesistitle:" Faculty Information System"
+},
+{
+    thesistitle:"Facilitating Research with Learner Data in Online Speaking Test"
+},
+{
+    thesistitle:"Class Time Table in Android"
+},
+{
+    thesistitle:"Interactive World Map Natural Disasters"
+},
+{
+    thesistitle:" Math Games for Algebra"
+},
+
+{
+    thesistitle:"Mobile Store Management System"
+},
+{
+    thesistitle:"Polling System"
+},
+{
+    thesistitle:"Pharmacy Management System"
+},
+{
+    thesistitle:"Payroll System"
+},
+{
+    thesistitle:"SSG Management System with SMS Notificatio"
+},
+{
+    thesistitle:" Wireless Projects"
+},
+{
+    thesistitle:"Philippine History App with Admin Panel"
+},
+{
+    thesistitle:"Location Based Friend Finder with SOS Alert Feature"
+},
+{
+    thesistitle:"Android Based IQ Test App with Score Monitoring"
+},
+{
+    thesistitle:"Web and Mobile Based Test Banking System with Item Analysis using PHP, MySQL and Framework7"
+},
+
+{
+    thesistitle:"Online voting system"
+},
+
+{
+    thesistitle:"Android Based IQ Test App with Score Monitoring"
+},
+
+{
+    thesistitle:"Supply inventory system"
+},
+
+{
+    thesistitle:"MemoDroid: Web and Mobile Memo Notification App"
+},
+
+{
+    thesistitle:"Web Based Facility Management Information System"
+},
+
+{
+    thesistitle:"RFID based Airport Luggage Security Scanning System"
+},
+
+{
+    thesistitle:"Touchscreen Point of Sale Application"
+},
+{
+    thesistitle:"Profile Management System of Employees."
+},
+{
+    thesistitle:"Business Use Cases for Accounting Apps"
+},
+{
+    thesistitle:" A New Hybrid Technique For Data Encryption"
+},
+{
+    thesistitle:"Diagnostic Centre Client Coordination System"
+},
+
+
+
+
+
+
+
+
+
+
+
+]
+
+export default TitleData;
